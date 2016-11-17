9f26a528249ab0d44bc20390546ad20ece2a96d1
YourBoyE (Imac) <egcrossmover@gmail.com>
2016-11-17 12:51:26 -0800

    finished the project, added more information for the pokemonDetailsVC

--- pokedex3/Pokemon.swift
+++ pokedex3/Pokemon.swift
@@ -20,8 +20,32 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolutionTxt: String!
+    private var _nextEvolutionName: String!
+    private var _nextEvolutionId: String!
+    private var _nextEvolutionLevel: String!
     private var _pokemonURL: String!
-
+    
+    var nextEvolutionLevel: String {
+        if _nextEvolutionLevel == nil {
+            _nextEvolutionLevel = ""
+        }
+        return _nextEvolutionLevel
+    }
+    
+    var nextEvolutionId: String {
+        if _nextEvolutionId == nil {
+            _nextEvolutionId = ""
+        }
+        return _nextEvolutionId
+    }
+    
+    var nextEvolutionName: String {
+        if _nextEvolutionName == nil {
+            _nextEvolutionName = ""
+        }
+        return _nextEvolutionName
+    }
+    
     var nextEvolutionTxt: String {
         if _nextEvolutionTxt == nil {
             _nextEvolutionTxt = ""
@@ -59,9 +83,9 @@ class Pokemon {
     }
     
     var type: String {
-       if _type == nil {
-        _type = ""
-    }
+        if _type == nil {
+            _type = ""
+        }
         return _type
     }
     
@@ -83,14 +107,14 @@ class Pokemon {
     init(name: String, pokedexId: Int) {
         self._name = name
         self._pokedexId = pokedexId
-     
+        
         self._pokemonURL = "\(URL_BASE)\(URL_POKEMON)\(self.pokedexId)/"
     }
     
     func downloadPokemonDetails(completed: @escaping DownloadComplete) {
         
         Alamofire.request(_pokemonURL).responseJSON { response in
-        
+            
             if let dict = response.result.value as? Dictionary<String, AnyObject> {
                 if let weight = dict["weight"] as? String {
                     self._weight = weight
@@ -127,16 +151,81 @@ class Pokemon {
                         }
                     }
                     
-                   self._type = names
+                    self._type = names
                     print(names)
                     
-                    } else {
+                } else {
+                    
+                    self._type = ""
+                }
+                
+                if let descArray = dict["descriptions"] as? [Dictionary<String, String>]
+                    , descArray.count > 0 {
+                    
+                    if let url = descArray[0]["resource_uri"] {
                         
-                        self._type = ""
+                        let descURL = "\(URL_BASE)\(url)"
+                        
+                        Alamofire.request(descURL).responseJSON (completionHandler: { (response) in
+                            
+                            if let descDict = response.result.value as? Dictionary<String, AnyObject> {
+                                
+                                if let description = descDict["description"] as? String {
+                                    
+                                    // replacing typos ::: the api had a typ :::
+                                    let newDescription = description.replacingOccurrences(of: "POKMON", with: "Pokemon")
+                                    
+                                    self._description = newDescription
+                                    print(newDescription)
+                                }
+                            }
+                            completed()
+                        })
+                    }
+                } else {
+                    
+                    self._description = ""
+                    
+                }
+              
+                if let evolutions = dict["evolutions"] as? [Dictionary<String, AnyObject>] , evolutions.count > 0 {
+                    
+                    if let nextEvo = evolutions[0]["to"] as? String {
+                        
+                        if nextEvo.range(of: "mega") == nil {
+                            
+                            self._nextEvolutionName = nextEvo
+                            
+                            if let uri = evolutions[0]["resource_uri"] as? String {
+                                
+                                let newStr = uri.replacingOccurrences(of: "/api/v1/pokemon/", with: "")
+                                
+                                let nextEvoId = newStr.replacingOccurrences(of: "/", with: "")
+                                
+                                self._nextEvolutionId = nextEvoId
+                                
+                                if let lvlExist = evolutions[0]["level"] {
+                                    
+                                    if let lvl = lvlExist as? Int {
+                                    
+                                        self._nextEvolutionLevel = "\(lvl)"
+                                    }
+                                    
+                                } else {
+                                    
+                                    self._nextEvolutionLevel = ""
+                                }
+                            }
                         }
                     }
+                    
+                    print(self.nextEvolutionLevel)
+                    print(self.nextEvolutionName)
+                    print(self.nextEvolutionId)
+                    
                 }
-             completed()   
+                
             }
+        }
     }
-    
+}
--- pokedex3/PokemonDetailVC.swift
+++ pokedex3/PokemonDetailVC.swift
@@ -22,6 +22,7 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet weak var pokedexLbl: UILabel!
     @IBOutlet weak var weightLbl: UILabel!
     @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
     
     @IBOutlet weak var currentEvoImg: UIImageView!
     @IBOutlet weak var nextEvoImg: UIImageView!
@@ -39,21 +40,37 @@ class PokemonDetailVC: UIViewController {
         mainImg.image = img
         currentEvoImg.image = img
         pokedexLbl.text = "\(pokemon.pokedexId)"
-        nextEvoImg.image = UIImage(named: "\(pokemon.pokedexId+1)")
         
         pokemon.downloadPokemonDetails {
-            print("Did arrive here?")
             self.updateUI()
         }
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        self.updateUI()
+    }
+    
     func updateUI() {
-        
+        print(" ENTERED UIUPDATE ()")
         attackLbl.text = pokemon.attack
         defenseLbl.text = pokemon.defense
         heightLbl.text = pokemon.height
         weightLbl.text = pokemon.weight
+        typeLbl.text = pokemon.type
+        descriptionLbl.text = pokemon.description
+        
+        if pokemon.nextEvolutionId == "" {
+            
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.isHidden = true
+        } else {
+            
+            nextEvoImg.isHidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            let str = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+            evoLbl.text = str
+        }
         
         
     }
